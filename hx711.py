2fdc092fb194d1e51cd0a4aba872bde0979aa6ad
rmuhlack <richard.muhlack@gmail.com>
2017-12-29 00:41:24 +1030

    debug

--- hx711.py
+++ hx711.py
@@ -15,7 +15,7 @@ class HX711:
         self.REFERENCE_UNIT = 1  # The value returned by the hx711 that corresponds to your reference unit AFTER dividing by the SCALE.
         
         self.OFFSET = 1
-        self.lastVal = long(0)
+        self.lastVal = int(0)
 
         self.LSByte = [2, -1, -1]
         self.MSByte = [0, 3, 1]
