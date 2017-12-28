bd21e985d757b101f67ddfa9cf4ecea10598a39e
rmuhlack <richard.muhlack@gmail.com>
2017-12-29 00:48:00 +1030

    debug

--- example.1.py
+++ example.1.py
@@ -26,7 +26,7 @@ hx.set_reading_format("LSB", "MSB")
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
-hx.set_reference_unit(0)
+hx.set_reference_unit(1)
 
 hx.reset()
 hx.tare()
