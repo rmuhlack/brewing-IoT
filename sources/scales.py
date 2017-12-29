29c215f38d1598bd23c50f46c2755b00571e57d2
surpdeh <roger@venning.net>
2017-12-29 15:52:32 +1000

    Refactor hardware into separate modules

---
+++ sources/scales.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+from hx711 import HX711
+
+
+
+def initialise(config):
+    """Initialise load cells hardware and tare"""
+    # config parameters should be available as config['??']
+    global __hx__
+    __hx__ = HX711(5, 6)
+
+    # HOW TO CALCULATE THE REFFERENCE UNIT
+    # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+    # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+    # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+    # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+    #hx.set_reference_unit(113)
+    hx.set_reference_unit(-22.535)
+
+    # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
+    # Still need to figure out why does it change.
+    # If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
+    # There is some code below to debug and log the order of the bits and the bytes.
+    # The first parameter is the order in which the bytes are used to build the "long" value.
+    # The second paramter is the order of the bits inside each byte.
+    # According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
+    __hx__.set_reading_format("LSB", "MSB")
+
+    __hx__.reset()
+    __hx__.tare()
+
+
+def cleanup():
+    GPIO.cleanup()
+
+
+def getWeightDelta():
+    # the HX711 author reports that if the sensor has been asleep for some time it is best to reset prior
+    global __hx__
+
+    __hx__.reset()
+    val = hx.get_weight(5)
+
+    # power down in case we are trying to be power conscious
+    __hx__.power_down()
+
+
+if __name__ == "__main__":
+    import sys
+    print "Should have a test harness here"
+
---
+++ sources/thermometer.py
@@ -0,0 +1,32 @@
+import time
+import Adafruit_GPIO
+
+# Local Imports
+from Adafruit_MAX31856 import MAX31856 as MAX31856
+
+
+def initialise(config):
+    """Initialise thermocouple hardware"""
+    global __sensor__
+
+    software_spi = {"clk": 11, "cs": 22, "do": 9, "di": 10}
+    sensor = MAX31856(software_spi=software_spi)
+
+def cleanup():
+    # no known clean up tasks for teh MAX31856 or Adafruit_GPIO?
+
+
+def getProbeTemperature():
+    global __sensor__
+
+    return __sensor__.read_temp_c()
+
+def getInternalTemperature():
+    global __sensor__
+
+    return __sensor__.read_internal_temp_c()
+
+
+if __name__ == "__main__":
+    import sys
+    print "Should have a test harness here"
